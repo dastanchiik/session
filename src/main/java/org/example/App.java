c905bb11805106be49dbb5eb46f1ebcd6ebe4c30
dastan <dosyafree32@gmail.com>
2023-02-03 22:35:50 +0600

    hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,6 @@ import org.example.repository.PersonImpl;
 public class App {
     public static void main(String[] args) {
         PersonImpl person1 = new PersonImpl();
-
+        System.out.println(person1.getById(4L));
     }
 }
