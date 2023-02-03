523881d75010165cb339c36245df0993a0e5541c
dastan <dosyafree32@gmail.com>
2023-02-03 22:32:16 +0600

    hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,10 +6,7 @@ import org.example.repository.PhoneImpl;
 
 public class App {
     public static void main(String[] args) {
-        Phone phone = new Phone("Redmin note 8+ pro max mega ucra",45);
-        Person person = new Person("Elmirdev",15,"elmir@gmail.com",phone);
         PersonImpl person1 = new PersonImpl();
-        PhoneImpl phone1 = new PhoneImpl();
-        person1.save(person);
+
     }
 }
