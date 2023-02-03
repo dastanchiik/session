afe7dab0e2f31642454f25a8647abb2bf148a837
dastan <dosyafree32@gmail.com>
2023-02-03 22:33:27 +0600

    hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,5 @@
 package org.example;
-import org.example.model.Person;
-import org.example.model.Phone;
 import org.example.repository.PersonImpl;
-import org.example.repository.PhoneImpl;
 
 public class App {
     public static void main(String[] args) {
