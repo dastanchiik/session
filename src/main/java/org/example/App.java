bba9e45877e64eb7762237443dc939bd7851cce1
dastan <dosyafree32@gmail.com>
2023-02-04 14:52:04 +0600

    hibernate not

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,3 +1,10 @@
-package org.example;import org.example.repository.PersonImpl;public class App { public static void main(String[] args) {PersonImpl person1 = new PersonImpl();System.out.println(person1.getById(4L));
+package org.example;
+
+import org.example.repository.PersonImpl;
+
+public class App {
+    public static void main(String[] args) {
+        PersonImpl person1 = new PersonImpl();
+        System.out.println(person1.getById(4L));
     }
 }
